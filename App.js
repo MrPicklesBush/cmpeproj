6c0d02daad5569cc3a4268fd7b2cb03029d46470
MrPicklesBush <bush.nguyen02@gmail.com>
2023-10-30 11:27:01 -0700

    create register page and implemented firebase to store user info

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { initializeApp } from 'firebase/app'; // Import initializeApp
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDM5cPvTKm78hmKivndsB0g0Ok2DuNjVnk",
+  authDomain: "cmpe131proj.firebaseapp.com",
+  projectId: "cmpe131proj",
+  storageBucket: "cmpe131proj.appspot.com",
+  messagingSenderId: "167110044900",
+  appId: "1:167110044900:web:ce47fca8d500bc260d10f1",
+  measurementId: "G-P78XFVKW6S"
+};
+
+const app = initializeApp(firebaseConfig); 
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
 
 const Stack = createStackNavigator();
 export default function App() {
@@ -12,6 +27,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Landing">
         <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false}}/>
+        <Stack.Screen name="Register" component={RegisterScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- components/auth/Register.js
+++ components/auth/Register.js
@@ -1,47 +1,57 @@
-import React, { Component } from 'react'
-import { View, Button, TextInput } from 'react-native'
+import React, { Component } from 'react';
+import { View, Button, TextInput } from 'react-native';
+
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'; // Import necessary Firebase functions
+import { getDatabase, ref, set } from 'firebase/database';
+import app from 'firebase/app'; // Import your initialized Firebase app
 
 export class Register extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            email: '',
-            password: '',
-            name: '',
-        }
+    this.state = {
+      email: '',
+      password: '',
+      name: '',
+    };
 
-        this.onSignUp = this.onSignUp.bind(this)
-    }
+    this.onSignUp = this.onSignUp.bind(this);
+  }
 
-    onSignUp(){
+  onSignUp() {
+    const { email, password, name } = this.state;
+    const auth = getAuth(app); // Get the authentication instance from your Firebase app
 
-    }
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-    render() {
+  render() {
     return (
       <View>
         <TextInput
-            placeholder="name"
-            onChangeText={(name) => this.setState({ name })}
+          placeholder="name"
+          onChangeText={(name) => this.setState({ name })}
         />
         <TextInput
-            placeholder="email"
-            onChangeText={(email) => this.setState({ email })}
+          placeholder="email"
+          onChangeText={(email) => this.setState({ email })}
         />
         <TextInput
-            placeholder="password"
-            secureTextEntry={true}
-            onChangeText={(password) => this.setState({ password })}
+          placeholder="password"
+          secureTextEntry={true}
+          onChangeText={(password) => this.setState({ password })}
         />
 
-        <Button 
-            onPress={() => this.onSignUp()}
-            title="Sign Up"
-        />
+        <Button onPress={() => this.onSignUp()} title="Sign Up" />
       </View>
-    )
+    );
   }
 }
 
-export default Register
+export default Register;
